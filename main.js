dbbc347b3edfa406da5bac29b2046d490990de71
Rui Zheng <cxzhengrui@foxmail.com>
2020-03-04 02:32:51 -0500

    Updated with the database schema discussed on 3/3

--- main.js
+++ main.js
@@ -15,22 +15,108 @@ firebase.analytics();
 
 
 var db = firebase.firestore();
-loadList();
+loadAllList();
 
-function loadList() {
+// Test for object, useless now
+// //object Item
+// class Item {
+//     constructor (product, quantity, prioirty, purchased, deleted ) {
+//         this.product = product;
+//         this.quantity = quantity;
+//         this.priority = prioirty;
+//         this.purchased = purchased;
+//         this.deleted = deleted;
+//     }
+//     toString() {
+//         return this.product + ', ' + this.quantity + ', ' + this.priority + ', ' + this.purchased + ', '+ this.deleted;
+//     }
+// }
+//
+// // Firestore data converter
+// itemConverter = {
+//     toFirestore: function(item) {
+//         return {
+//             product : item.product,
+//             quantity : item.quantity,
+//             priority : item.priority,
+//             purchased : item.purchased,
+//             deleted : item.deleted
+//         }
+//     },
+//     fromFirestore: function(snapshot, options){
+//         const data = snapshot.data(options);
+//         return new Item(data.product, data.quantity, data.priority, data.purchased, data.deleted)
+//     }
+// }
+
+function addNewItem() {
+    var pr = document.getElementById("newItemPrioritySelect").value;
+    var pn = document.getElementById("itemName").value;
+    var po = document.getElementById("itemNumber").value;
+
+    if (!pn) {
+        window.alert("Item name cannot be empty!")
+        return
+    }
+    if (isNaN(po) || po == null) {
+        po = 1;
+        window.alert(po)
+    }
+
+    var newAddID;
+
+
+    db.collection("Item").add({
+        product: pn,
+        quantity: parseInt(po),
+        priority: parseInt(pr),
+        purchased: false,
+        deleted: false
+    }).then(function (docRef) {
+        newAddID = docRef.id;
+    }).catch(function (error) {
+        window.error("Error adding document: ", error);
+    });
+
+    document.getElementById("newItem_form").reset();
+
     var table = document.getElementById("userItemList");
 
-    db.collection("userlists").get().then(function(querySnapshot) {
+    var row = table.insertRow(-1);
+
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+
+    var checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.name = "cb_newAddID";
+    checkbox.id = "cb_newAddID";
+
+    cell1.appendChild(checkbox);
+    cell2.appendChild(document.createTextNode(pn));
+    cell3.appendChild(document.createTextNode(po));
+    cell4.appendChild(document.createTextNode(checkPirority(pr)));
+
+
+}
+
+function loadAllList() {
+    var table = document.getElementById("userItemList");
+
+    db.collection("Item").get().then(function (querySnapshot) {
         querySnapshot.forEach(function(doc) {
             // doc.data() is never undefined for query doc snapshots
             var product = doc.data();
             var proId = doc.id;
-            var proP = product.Priority;
-            var proPriority = checkPirority(proP);
 
-            var proName = product.Product;
-            var proQ = product.Quantity;
-            var proPurchased = product.Purchased;
+            var proP = product.priority;
+            var proPriority = checkPirority(proP);
+            var proName = product.product;
+            var proQ = product.quantity;
+            var proPurchased = product.purchased;
+            var proDelete = product.delete;
 
             var row = table.insertRow(-1);
 
@@ -69,56 +155,7 @@ function checkPirority(i) {
     }
 }
 
-function addNewItem() {
-    var pr = document.getElementById("newItemPrioritySelect").value;
-    var pn = document.getElementById("itemName").value;
-    var po = document.getElementById("itemNumber").value;
 
-    if(!pn) {
-        window.alert("Item name cannot be empty!")
-    }
-    if(!isNaN(po) || po == null) {
-        po = 1;
-    }
-
-    var newAddID;
-
-    db.collection("userlists").add({
-        Priority: parseInt(pr),
-        Product: pn,
-        Purchased: false,
-        Quantity: parseInt(po)
-
-    }).then(function(docRef) {
-        newAddID = docRef.id;
-    }).catch(function(error) {
-            window.error("Error adding document: ", error);
-        });
-
-    document.getElementById("newItem_form").reset();
-
-    var table = document.getElementById("userItemList");
-
-    var row = table.insertRow(-1);
-
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-
-    var checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
-    checkbox.name = "cb_newAddID";
-    checkbox.id = "cb_ID";
-
-    cell1.appendChild(checkbox);
-    cell2.appendChild(document.createTextNode(pn));
-    cell3.appendChild(document.createTextNode(po));
-    cell4.appendChild(document.createTextNode(checkPirority(pr)));
-
-
-
-}
 
 $(function () {
     $('#addItem').popover({
